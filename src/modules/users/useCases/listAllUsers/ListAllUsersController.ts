34ab475f9aa35cb90531d973ab79380cc5205120
Tiago Dantas <tiago.dantas16@live.com>
2021-11-02 22:16:27 -0300

    Criação da documentação usando o Swagger

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -12,7 +12,7 @@ class ListAllUsersController {
       const user = this.listAllUsersUseCase.execute({
         user_id: String(user_id),
       });
-      return response.status(201).json(user);
+      return response.status(200).json(user);
     } catch (error) {
       return response.status(400).json({ error: error.message });
     }
